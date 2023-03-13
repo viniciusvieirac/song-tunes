794af40a5ee437ffeddb90e103a162bfb10820cf
vieira <vini.clau0@gmail.com>
2023-03-13 16:36:48 -0300

    faz requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact component={ NotFound } />
+        </Switch>
+
+      </div>
+
+    );
   }
 }
 
 export default App;
-
-// vasco da gama
