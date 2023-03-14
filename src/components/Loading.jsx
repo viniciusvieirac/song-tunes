0973f21e6ca706162852112e5b6830975636bda0
vieira <vini.clau0@gmail.com>
2023-03-14 17:07:46 -0300

    faz requisito 6

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <h1>Carregando...</h1>
+      <h1 className="loading">Carregando...</h1>
     );
   }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
-      name: '',
+      artist: '',
+      artistName: '',
       disabledButton: true,
+      loading: false,
+      albuns: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.validateForm = this.validateForm.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { artist } = this.state;
+    this.setState({
+      loading: true,
+      albuns: [],
+      error: '',
+    });
+    const response = await searchAlbumsAPI(artist);
+    if (response.length === 0) {
+      this.setState({
+        error: 'Nenhum álbum foi encontrado',
+        loading: false,
+      });
+    } else {
+      this.setState({
+        albuns: response,
+        loading: false,
+        artist: '',
+        artistName: artist,
+      });
+    }
+  };
 
   handleChange({ target }) {
     const { value } = target;
     this.setState({
-      name: value,
+      artist: value,
     }, this.validateForm);
   }
 
   validateForm() {
-    const { name } = this.state;
+    const { artist } = this.state;
     const MIN_VALUE = 2;
-    if (name.length >= MIN_VALUE) {
+    if (artist.length >= MIN_VALUE) {
       this.setState({
         disabledButton: false,
       });
@@ -34,17 +65,21 @@ class Search extends Component {
   }
 
   render() {
-    const { name, disabledButton } = this.state;
+    const { artist, disabledButton, loading, albuns, artistName, error } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            value={ name }
-            type="text"
-            onChange={ this.handleChange }
-            data-testid="search-artist-input"
-          />
+        <form onSubmit={ this.handleSubmit }>
+          <label>
+            Buscar albuns de:
+            <input
+              value={ artist }
+              type="text"
+              onChange={ this.handleChange }
+              data-testid="search-artist-input"
+              placeholder="Digite o nome do artista"
+            />
+          </label>
           <button
             type="submit"
             disabled={ disabledButton }
@@ -53,6 +88,39 @@ class Search extends Component {
             Pesquisar
 
           </button>
+          {loading && <Loading />}
+          {error && <p>{error}</p>}
+          {albuns.length > 0 && (
+            <div>
+              <p>
+                Resultado de álbuns de:
+                {' '}
+                {artistName}
+              </p>
+              <ul>
+                {albuns.map((album) => (
+                  <div key={ album.collectionName }>
+
+                    <li
+                      className="album"
+                      key={ album.collectionName }
+                    >
+                      <Link
+                        to={ `/album/${album.collectionId}` }
+                        data-testid={ `link-to-album-${album.collectionId}` }
+                      >
+                        <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                        <p>
+                          {' '}
+                          {album.collectionName}
+                        </p>
+                      </Link>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          )}
         </form>
       </div>
     );
