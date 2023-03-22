6ca7e011a3ad8aaa59bd614f9a17f17e54171ba3
vieira <vini.clau0@gmail.com>
2023-03-22 20:10:33 -0300

    req 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -23,12 +23,14 @@ class ProfileEdit extends Component {
 
   fetchUser = async () => {
     const { name, email, description, image } = await getUser();
+    const validateUser = name && email && description && image;
     this.setState({
       loading: false,
       nome: name,
       email,
       description,
       image,
+      disabledButton: !validateUser,
     });
   };
 
@@ -43,14 +45,18 @@ class ProfileEdit extends Component {
     const { name, value } = target;
     this.setState({
       [name]: value,
-    }, this.validateForm());
+    }, this.validateForm);
   };
 
   validateForm = () => {
     const { nome, email, description, image } = this.state;
-    if (nome !== '' && email !== '' && description !== '' && image !== '') {
+    if (nome && email && description && image) {
       this.setState({
         disabledButton: false,
+      });
+    } else {
+      this.setState({
+        disabledButton: true,
       });
     }
   };
@@ -60,60 +66,63 @@ class ProfileEdit extends Component {
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        {loading && <Loading />}
-        <form>
-          <label htmlFor="image">
-            Envie sua imagem:
-            <input
-              type="url"
-              value={ image }
-              name="image"
-              onChange={ this.handleChange }
-              data-testid="edit-input-image"
-            />
-          </label>
-          <label htmlFor="nome">
-            Altere seu nome:
-            <input
-              type="text"
-              value={ nome }
-              name="nome"
-              data-testid="edit-input-name"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="email">
-            Digite seu email:
-            <input
-              type="email"
-              value={ email }
-              name="email"
-              data-testid="edit-input-email"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="description">
-            Descrição:
-            <textarea
-              name="description"
-              value={ description }
-              id=""
-              cols="30"
-              rows="10"
-              data-testid="edit-input-description"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="edit-button-save"
-            disabled={ disabledButton }
-            onClick={ this.handleClick }
-          >
-            Editar perfil
+        {loading ? <Loading /> : (
 
-          </button>
-        </form>
+          <form>
+            <label htmlFor="image">
+              Envie sua imagem:
+              <input
+                type="url"
+                value={ image }
+                name="image"
+                onChange={ this.handleChange }
+                data-testid="edit-input-image"
+              />
+            </label>
+            <label htmlFor="nome">
+              Altere seu nome:
+              <input
+                type="text"
+                value={ nome }
+                name="nome"
+                data-testid="edit-input-name"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="email">
+              Digite seu email:
+              <input
+                type="email"
+                value={ email }
+                name="email"
+                data-testid="edit-input-email"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="description">
+              Descrição:
+              <textarea
+                name="description"
+                value={ description }
+                id=""
+                cols="30"
+                rows="10"
+                data-testid="edit-input-description"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="edit-button-save"
+              disabled={ disabledButton }
+              onClick={ this.handleClick }
+            >
+              Editar perfil
+
+            </button>
+          </form>
+
+        )}
       </div>
     );
   }
